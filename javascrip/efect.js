e2175bb84518d81672d3557e34f746b6f0eab1f2
alejandro asess <101218762+aleaases@users.noreply.github.com>
2022-12-29 14:24:27 -0300

    master commit actulizacion terminada

--- javascrip/efect.js
+++ javascrip/efect.js
@@ -1,6 +1,2 @@
-//Using Vanilla JS
-document.querySelector(".hamburguer").addEventListener("click", function () {
-    document.querySelector(".full-menu").classList.toggle("active");
-    document.querySelector(".hamburguer").classList.toggle("close-hamburguer");
-});
+
 
